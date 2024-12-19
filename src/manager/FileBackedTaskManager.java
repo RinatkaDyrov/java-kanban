492c96bb64aeb1f3293c1c84acc822a9b02670aa
rinatka_dyrov <polaroid264@yandex.ru>
2024-12-19 12:01:52 +0300

    Sprint 7 in process Adding FileBackedTaskManager

---
+++ src/manager/FileBackedTaskManager.java
@@ -0,0 +1,10 @@
+package manager;
+
+public class FileBackedTaskManager extends InMemoryTaskManager{
+
+    public FileBackedTaskManager(HistoryManager historyManager) {
+        super(historyManager);
+    }
+
+
+}
