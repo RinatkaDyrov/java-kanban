db6cc1e434880f566109a055b5ccf4f97d973d90
rinatka_dyrov <poalroid264@yandex.ru>
2024-12-26 13:09:38 +0300

    Final project num 7 is completed Added FileBackedTaskManager Added new tests Fixed Main class

--- src/Main.java
+++ src/Main.java
@@ -9,90 +9,89 @@ import model.Task;
 import java.io.File;
 import java.io.IOException;
 
-public static void main(String[] args) throws IOException {
-    File file = File.createTempFile("history", ".csv");
-    file.deleteOnExit();
-    FileBackedTaskManager manager = Managers.getFileBackedTaskManager(file);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File file = File.createTempFile("history", ".csv");
+        file.deleteOnExit();
+        FileBackedTaskManager manager = Managers.getFileBackedTaskManager(file);
 
-    // блок проверки создания объектов
-    Task task1 = new Task("First task", "First task desc", Status.NEW);
-    Task task2 = new Task("Second task", "Second task desc", Status.IN_PROGRESS);
-    manager.createTask(task1);
-    manager.createTask(task2);
+        Task task1 = new Task("First task", "First task desc", Status.NEW);
+        Task task2 = new Task("Second task", "Second task desc", Status.IN_PROGRESS);
+        manager.createTask(task1);
+        manager.createTask(task2);
 
-    Epic epic1 = new Epic("First epic", "First epic desc");
-    manager.createEpic(epic1);
+        Epic epic1 = new Epic("First epic", "First epic desc");
+        manager.createEpic(epic1);
 
-    Subtask subtask1 = new Subtask("First subtask e1", "First subtask e1 desc", Status.NEW, epic1.getId());
-    Subtask subtask2 = new Subtask("Second subtask e1", "Second subtask e1 desc", Status.NEW, epic1.getId());
-    Subtask subtask3 = new Subtask("Third subtask e1", "Third subtask e2 desc", Status.NEW, epic1.getId());
+        Subtask subtask1 = new Subtask("First subtask e1", "First subtask e1 desc", Status.NEW, epic1.getId());
+        Subtask subtask2 = new Subtask("Second subtask e1", "Second subtask e1 desc", Status.NEW, epic1.getId());
+        Subtask subtask3 = new Subtask("Third subtask e1", "Third subtask e2 desc", Status.NEW, epic1.getId());
 
-    manager.createSubtask(subtask1);
-    manager.createSubtask(subtask2);
-    manager.createSubtask(subtask3);
-    printAllTasksByManager(manager);
+        manager.createSubtask(subtask1);
+        manager.createSubtask(subtask2);
+        manager.createSubtask(subtask3);
+        printAllTasksByManager(manager);
 
-    // блок проверки изменения объектов
-    manager.getTaskById(1);
-    manager.getTaskById(2);
-    manager.getTaskById(1);
-    manager.getTaskById(1);
-    manager.getTaskById(1);
-    manager.getTaskById(2);
-    manager.getTaskById(2);
-    manager.getTaskById(1);
+        // блок проверки отработки менеджера истории
+        manager.getTaskById(1);
+        manager.getTaskById(2);
+        manager.getTaskById(1);
+        manager.getTaskById(1);
+        manager.getTaskById(1);
+        manager.getTaskById(2);
+        manager.getTaskById(2);
+        manager.getTaskById(1);
 
-    manager.getSubtaskById(4);
-    manager.getSubtaskById(5);
-    manager.getSubtaskById(5);
-    manager.getSubtaskById(6);
-    manager.getSubtaskById(6);
-    manager.getSubtaskById(4);
-    manager.getSubtaskById(5);
-    manager.getSubtaskById(4);
-    manager.getSubtaskById(6);
-    manager.getSubtaskById(4);
+        manager.getSubtaskById(4);
+        manager.getSubtaskById(5);
+        manager.getSubtaskById(5);
+        manager.getSubtaskById(6);
+        manager.getSubtaskById(6);
+        manager.getSubtaskById(4);
+        manager.getSubtaskById(5);
+        manager.getSubtaskById(4);
+        manager.getSubtaskById(6);
+        manager.getSubtaskById(4);
 
-    System.out.println("\n");
-    printAllTasks(manager);
-
-    System.out.println("\n\n");
-    printAllTasks(manager);
-}
-
-public static void printAllTasksByManager(TaskManager manager) {
-    System.out.println("Список задач:");
-    manager.getAllTasks().forEach(System.out::println);
-    System.out.println("Список эпиков:");
-    manager.getAllEpics().forEach(System.out::println);
-    System.out.println("Список подзадач по эпикам:");
-    manager.getAllEpics().forEach(epic -> {
-        System.out.println("Эпик " + epic.getId() + ": ");
-        epic.getSubtasks().forEach(System.out::println);
-    });
-    System.out.println("==============================================");
-}
-
-private static void printAllTasks(TaskManager manager) {
-    System.out.println("Задачи:");
-    for (Task task : manager.getAllTasks()) {
-        System.out.println(task);
+        System.out.println("\n");
+        printAllTasks(manager);
     }
-    System.out.println("Эпики:");
-    for (Task epic : manager.getAllEpics()) {
-        System.out.println(epic);
 
-        for (Task task : manager.getAllSubtaskByEpic(epic.getId())) {
-            System.out.println("--> " + task);
+
+    public static void printAllTasksByManager(TaskManager manager) {
+        System.out.println("Список задач:");
+        manager.getAllTasks().forEach(System.out::println);
+        System.out.println("Список эпиков:");
+        manager.getAllEpics().forEach(System.out::println);
+        System.out.println("Список подзадач по эпикам:");
+        manager.getAllEpics().forEach(epic -> {
+            System.out.println("Эпик " + epic.getId() + ": ");
+            epic.getSubtasks().forEach(System.out::println);
+        });
+        System.out.println("==============================================");
+    }
+
+    private static void printAllTasks(TaskManager manager) {
+        System.out.println("Задачи:");
+        for (Task task : manager.getAllTasks()) {
+            System.out.println(task);
         }
-    }
-    System.out.println("Подзадачи:");
-    for (Task subtask : manager.getAllSubtasks()) {
-        System.out.println(subtask);
-    }
+        System.out.println("Эпики:");
+        for (Task epic : manager.getAllEpics()) {
+            System.out.println(epic);
 
-    System.out.println("История:");
-    for (Task task : manager.getHistory()) {
-        System.out.println(task);
+            for (Task task : manager.getAllSubtaskByEpic(epic.getId())) {
+                System.out.println("--> " + task);
+            }
+        }
+        System.out.println("Подзадачи:");
+        for (Task subtask : manager.getAllSubtasks()) {
+            System.out.println(subtask);
+        }
+
+        System.out.println("История:");
+        for (Task task : manager.getHistory()) {
+            System.out.println(task);
+        }
     }
 }
